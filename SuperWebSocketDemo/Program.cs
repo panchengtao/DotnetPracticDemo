aa833ea5831921bd4de4d278147391df8e71724f
wddpct <1242450527@qq.com>
2016-07-03 19:35:39 +0800

    SuperWebSocketDemo提交

---
+++ SuperWebSocketDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using SuperSocket.SocketBase;
+using SuperWebSocket;
+using static System.Console;
+
+namespace SuperWebSocketDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var server = new WebSocketServer();
+
+            server.NewSessionConnected += ServerNewSessionConnected;
+            server.NewMessageReceived += ServerNewMessageRecevied;
+            server.SessionClosed += ServerSessionClosed;
+
+            try
+            {
+                server.Setup("127.0.0.1", 40001); 
+                server.Start(); 
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+            ReadKey();
+        }
+
+        private static void ServerSessionClosed(WebSocketSession session, CloseReason value)
+        {
+            WriteLine(session.Origin);
+        }
+
+        public static void ServerNewMessageRecevied(WebSocketSession session, string value)
+        {
+            WriteLine(value);
+            session.Send("已收到："+value);
+        }
+
+        /// <param name="session"></param>
+        public static void ServerNewSessionConnected(WebSocketSession session)
+        {
+            WriteLine(session.Origin);
+        }
+    }
+}
