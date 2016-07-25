10b3c3f080be05c7ffe332e43118f284219d1185
wddpct <1242450527@qq.com>
2016-07-25 21:09:04 +0800

    yield迭代器实例上传

---
+++ IterationDemo/IterationDemo/IterationSample.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+namespace IterationDemo
+{
+    public class IterationSample : IEnumerable
+    {
+        public readonly int StartingPoint;
+        public readonly object[] Values;
+
+        public IterationSample(object[] values, int startingPoint)
+        {
+            Values = values;
+            StartingPoint = startingPoint;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new IterationSampleEnumerator(this);
+        }
+    }
+
+    public sealed class IterationSampleEnumerator : IEnumerator
+    {
+        private readonly IterationSample _parent; //迭代的对象  #1
+        private int _position; //当前游标的位置 #2
+
+        internal IterationSampleEnumerator(IterationSample parent)
+        {
+            _parent = parent;
+            _position = -1;
+        }
+
+        public bool MoveNext()
+        {
+            if (_position != _parent.Values.Length) //判断当前位置是否为最后一个，如果不是游标自增 #4
+            {
+                _position++;
+            }
+
+            return _position < _parent.Values.Length;
+        }
+
+        public void Reset()
+        {
+            _position = -1;
+        }
+
+        public object Current {
+            get
+            {
+                if (_position == -1 || _position == _parent.Values.Length)//第一个之前和最后一个自后的访问非法 #5
+                {
+                    throw new InvalidOperationException();
+                }
+
+                Int32 index = _position + _parent.StartingPoint;//考虑自定义开始位置的情况  #6
+                index = index % _parent.Values.Length;
+                return _parent.Values[index];
+            }
+        }
+    }
+}
---
+++ IterationDemo/IterationDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationDemo
+{
+    class Program
+    {
+        static readonly String Padding = new String(' ', 30);
+        static IEnumerable<Int32> CreateEnumerable()
+        {
+            Console.WriteLine("{0} CreateEnumerable()方法开始", Padding);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("{0}开始 yield", i);
+                yield return i;
+                Console.WriteLine("{0}yield 结束", Padding);
+            }
+            Console.WriteLine("{0} Yielding最后一个值", Padding);
+            yield return -1;
+            Console.WriteLine("{0} CreateEnumerable()方法结束", Padding);
+        }
+
+        static void Main(string[] args)
+        {
+            IEnumerable<Int32> iterable = CreateEnumerable();
+            IEnumerator<Int32> iterator = iterable.GetEnumerator();
+            Console.WriteLine("开始迭代");
+            while (true)
+            {
+                Console.WriteLine("调用MoveNext方法……");
+                Boolean result = iterator.MoveNext();
+                Console.WriteLine("MoveNext方法返回的{0}", result);
+                if (!result)
+                {
+                    break;
+                }
+                Console.WriteLine("获取当前值……");
+                Console.WriteLine("获取到的当前值为{0}", iterator.Current);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
